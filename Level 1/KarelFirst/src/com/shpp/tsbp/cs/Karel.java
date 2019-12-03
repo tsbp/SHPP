9c432c6aadc50338339a9505eeadaddb719773e7
tsbp <tsbp82@gmail.com>
2019-12-03 22:50:14 +0200

    first

---
+++ Level 1/KarelFirst/src/com/shpp/tsbp/cs/Karel.java
@@ -0,0 +1,11 @@
+package com.shpp.tsbp.cs;
+
+import com.shpp.karel.KarelTheRobot;
+
+public class Karel extends KarelTheRobot {
+
+    public void run() throws Exception {
+        move();
+    }
+
+}
