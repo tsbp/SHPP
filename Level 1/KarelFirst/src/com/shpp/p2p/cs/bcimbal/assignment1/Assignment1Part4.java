4ebc28a4af0a76bf2a3fdd7eb323b507fc24d823
tsbp <tsbp82@gmail.com>
2019-12-05 20:59:44 +0200

    comments added

--- Level 1/KarelFirst/src/com/shpp/p2p/cs/bcimbal/assignment1/Assignment1Part4.java
+++ Level 1/KarelFirst/src/com/shpp/p2p/cs/bcimbal/assignment1/Assignment1Part4.java
@@ -10,36 +10,50 @@ public class Assignment1Part4 extends KarelTheRobot {
     public void run() throws Exception {
         //------------------------------------------------
         putBeeper();
-        while(frontIsClear()){
-           fillLine();
-        }
-        //------------------------------------------------
+        fillLine();
         turnLeft();
+        //------------------------------------------------
+
         while(frontIsClear()) {
             fillLine();
             turnAround();
-            while (frontIsClear()) move();
-            turnRight();
-            if(frontIsClear()){
-                move();
-                turnRight();
-            }
+            moveBack();
+            goToNextColumn();
         }
     }
 
     //==================================================================================================================
-    //
+    // To turn right Karel turns left triple
+    //==================================================================================================================
+    private void goToNextColumn() throws Exception{
+        turnRight();
+        if(frontIsClear()){
+            move();
+            turnRight();
+        }
+    }
+
+    //==================================================================================================================
+    // To turn right Karel turns left triple
+    //==================================================================================================================
+    private void moveBack() throws  Exception{
+        while (frontIsClear()) move();
+    }
+
+    //==================================================================================================================
+    // fiil line in current dirrection
+    // by alternating beepres and empties
     //==================================================================================================================
     private void fillLine() throws Exception{
         // fill column
-        while (frontIsClear()) {
-            if (noBeepersPresent()) {
-                if (frontIsClear()) {
-                    move();
-                    putBeeper();
-                }
-            } else {
-                if (frontIsClear()) move();
+        while (frontIsClear()) {              // while we not meet wall repeat
+            if (noBeepersPresent()) {         //     +  if beeper absent
+                if (frontIsClear()) {         //     +
+                    move();                   //     +          if no wall ahead make step and put beeper
+                    putBeeper();              //     +
+                }                             //     +
+            } else {                          //     +  if beeper present
+                if (frontIsClear()) move();   //     +          if no wall ahead make step
             }
         }
     }
