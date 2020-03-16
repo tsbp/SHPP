421de75eb36d509eff9ed692883a62441d1584b8
tsbp <tsbp82@gmail.com>
2020-03-16 23:57:21 +0200

    assig12

--- Level 12/assignment12/src/com/shpp/p2p/cs/bcimbal/assignment12/Assignment12Part1.java
+++ Level 12/assignment12/src/com/shpp/p2p/cs/bcimbal/assignment12/Assignment12Part1.java
@@ -10,82 +10,84 @@ import java.util.HashMap;
 
 public class Assignment12Part1 {
 
-    private static ArrayList<HashMap<Point, Boolean>> silhoettes = new ArrayList<>();
-    private static BufferedImage img;
+    private static ArrayList<HashMap<Point, Boolean>> silhouettes = new ArrayList<>();
 
-    private static final double THRES = 50;
+    private static final double LUMINANCE_THRESHOLD = 50;
+    private static final double MIN_SILHOUETTE_SIZE = 10;
+    //    -Xss128m
 
     public static void main(String[] args) {
-        img = readImageFromFile("assets\\test2.jpg");
-        int a = img.getWidth();
-        int b = img.getHeight();
-        int fon = img.getRGB(0, 0);
-        double lumFon = getLuminance(fon);
-        // double l = lum;
-        for (int i = 1; i < a -1; i++) {
-            for (int j = 1; j < b - 1; j++) {
-                double curLum = getLuminance(img.getRGB(i, j));
+        String filepath = "assets\\";
+        if(args.length > 0)
+            filepath = filepath + args[0];
+        else
+            filepath = filepath + "test.jpg";
+        BufferedImage img = readImageFromFile(filepath);
+        System.out.println("Silhoettes count (" + filepath + "): " + getSilhouettesCount(img));
+    }
 
-                if ((lumFon > curLum + THRES) && !belongsTo(i, j, silhoettes)) {
-                    {
-                        HashMap<Point, Boolean> silhouett = discoverSilhoet(lumFon, i, j);
-                        //if (silhouett.size() > 200)
-                            silhoettes.add(silhouett);
-                    }
+    private static int getSilhouettesCount(BufferedImage img) {
+        /* define background luminance at 0,0 */
+        double bgLuminance = getLuminance(img.getRGB(0, 0));
+        /* proceed each pixel*/
+        for (int x = 1; x < img.getWidth() - 1; x++) {
+            for (int y = 1; y < img.getHeight() - 1; y++) {
+                double curLum = getLuminance(img.getRGB(x, y));
+                if ((bgLuminance > curLum + LUMINANCE_THRESHOLD) && !belongsTo(x, y, silhouettes)) {
+                    HashMap<Point, Boolean> silhouett = discoverSilhoet(img, bgLuminance, x, y);
+                    if (silhouett.size() >= MIN_SILHOUETTE_SIZE)
+                        silhouettes.add(silhouett);
                 }
             }
         }
-        //Y = 0.2126 * R + 0.7152 * G + 0.0722 * B
-        System.out.println("count: " + silhoettes.size());
+        return silhouettes.size();
     }
 
     private static double getLuminance(int rgb) {
         Color col = new Color(rgb);
-        double a = 0.2126 * col.getRed() + 0.7152 * col.getGreen() + 0.0722 * col.getBlue();
-        return a;
+        /* https://en.wikipedia.org/wiki/Relative_luminance */
+        return 0.2126 * col.getRed() + 0.7152 * col.getGreen() + 0.0722 * col.getBlue();
     }
 
-    private static boolean belongsTo(int x, int y, ArrayList<HashMap<Point, Boolean>> silh) {
-        for (int i = 0; i < silh.size(); i++) {
-            if (silh.get(i).containsKey(new Point(x, y))) {
+    private static boolean belongsTo(int x, int y, ArrayList<HashMap<Point, Boolean>> silhouettes) {
+        for (HashMap<Point, Boolean> silhouet : silhouettes) {
+            if (silhouet.containsKey(new Point(x, y))) {
                 return true;
             }
         }
         return false;
     }
 
-    private static HashMap<Point, Boolean> discoverSilhoet(double fon, int x, int y) {
+    private static HashMap<Point, Boolean> discoverSilhoet(BufferedImage image, double fon, int x, int y) {
         HashMap<Point, Boolean> tmp = new HashMap<>();
-        dfs(x, y, fon, tmp);
+        dfs(x, y, image, fon, tmp);
         return tmp;
     }
 
-//    -Xss1024m
+    static int[][] searchMatrix = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
 
-    private static void dfs(int x, int y, double fon, HashMap<Point, Boolean> tmp) {
-        tmp.put(new Point(x, y), true);
+    private static void dfs(int coordX, int coordY, BufferedImage image, double fon, HashMap<Point, Boolean> map) {
+        map.put(new Point(coordX, coordY), true);
+        /* search unmarked pixels around current pixel*/
         for (int i = 0; i < searchMatrix.length; i++) {
-            int _x = x + searchMatrix[i][0];
-            int _y = y + searchMatrix[i][1];
-            if ((_x > 0 && _y > 0) && (_x < img.getWidth() && _y < img.getHeight())) {
-
-                double curLum = getLuminance(img.getRGB(_x, _y));
-                if (/*img.getRGB(_x, _y) != fon*/(fon > curLum + THRES)
-                        && !tmp.containsKey(new Point(_x, _y))) {
-                    dfs(_x, _y, fon, tmp);
+            int x = coordX + searchMatrix[i][0];
+            int y = coordY + searchMatrix[i][1];
+            /* prevent out of picture bounds*/
+            if ((x > 0 && y > 0) && (x < image.getWidth() && y < image.getHeight())) {
+                double curLum = getLuminance(image.getRGB(x, y));
+                if ((fon > curLum + LUMINANCE_THRESHOLD)
+                        && !map.containsKey(new Point(x, y))) {
+                    dfs(x, y, image, fon, map);
                 }
             }
         }
     }
 
-    static int[][] searchMatrix = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
-
-    private static BufferedImage readImageFromFile(String file) {
+    private static BufferedImage readImageFromFile(String filepath) {
         BufferedImage img = null;
         try {
-            File input_file = new File(file);
-            img = new BufferedImage(1, 1,
-                    BufferedImage.TYPE_INT_ARGB);
+            File input_file = new File(filepath);
+            img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
             img = ImageIO.read(input_file);
         } catch (IOException e) {
             System.out.println("Error: " + e);
